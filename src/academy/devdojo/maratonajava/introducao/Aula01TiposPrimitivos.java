3356bff13b22dc7b291a7a9f9df3a2c7bf071f1f
Mergs <gabrielsnmergulhao@gmail.com>
2024-07-31 17:24:41 -0300

    add Aula01TiposPrimitivos

---
+++ src/academy/devdojo/maratonajava/introducao/Aula01TiposPrimitivos.java
@@ -0,0 +1,22 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula01TiposPrimitivos {
+
+    public static void main(String[] args) {
+        //TIPOS PRIMITIVOS - int, double, float, char, byte, short, long, booleanddd
+        int age = 10;
+        long numeroGrande = 2000000000;
+        double salarioDouble = 2.000000002333244124;
+        float salarioFloat = 25.135F;
+        short salarioShort = 32767;
+        byte salarioByte = 127;
+        boolean verdadeiro = true;
+        boolean falso = false;
+        char caractere = 'a';
+
+        System.out.println("a idade é " + 10 + "anos");
+        System.out.println();
+        System.out.println();
+        System.out.println();
+    }
+}
