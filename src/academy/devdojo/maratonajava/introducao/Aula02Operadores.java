4e2c424428b3cdf31ba5769b52100b79b1124233
Mergs <gabrielsnmergulhao@gmail.com>
2024-07-31 18:04:26 -0300

    implements Aula02Operadores

--- src/academy/devdojo/maratonajava/introducao/Aula02Operadores.java
+++ src/academy/devdojo/maratonajava/introducao/Aula02Operadores.java
@@ -1,4 +1,31 @@
 package academy.devdojo.maratonajava.introducao;
 
 public class Aula02Operadores {
+    public static void main(String[] args) {
+        // + - / *
+
+        int numero01 = 10;
+        double numero02 = 20;
+        double resultado =  numero01 / numero02;
+        System.out.println(resultado);
+
+        //resto (%)
+        int resto = 25 % 2;
+        System.out.println(resto);
+
+        // lógicos < > <= >= == != (sempre retornam booleanos)
+
+        boolean isDezMaiorQueVinte = 10 > 20;
+        boolean isDezMenorQueVinte = 10 < 20;
+        boolean isDezIgualAVinte = 10 == 20;
+        boolean isDezDiferenteDeVinte = 10 != 20;
+
+        System.out.println("isDezMaiorQueVinte " + isDezMaiorQueVinte);
+        System.out.println("isDezMenorQueVinte " + isDezMenorQueVinte);
+        System.out.println("isDezIgualAVinte " + isDezIgualAVinte);
+        System.out.println("isDezDiferenteDeVinte " + isDezDiferenteDeVinte);
+
+
+
+    }
 }
