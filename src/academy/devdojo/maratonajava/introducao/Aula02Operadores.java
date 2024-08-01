777908c74d7c84a24a29bb42f70426c44f177d5e
Mergs <gabrielsnmergulhao@gmail.com>
2024-08-01 12:07:02 -0300

    implements Aula02Operadores

--- src/academy/devdojo/maratonajava/introducao/Aula02Operadores.java
+++ src/academy/devdojo/maratonajava/introducao/Aula02Operadores.java
@@ -25,7 +25,31 @@ public class Aula02Operadores {
         System.out.println("isDezIgualAVinte " + isDezIgualAVinte);
         System.out.println("isDezDiferenteDeVinte " + isDezDiferenteDeVinte);
 
+        // && -> (AND)        || -> (OR)        ! -> (NOT)
 
+        int idade = 35;
+        float salario = 3500F;
+        boolean isDentroDaLeiMaiorQueTrinta = idade >= 30 && salario >= 4612.25F;
+        boolean isDentroDaLeiMenorQueTrinta = idade <= 30 && salario >= 3500;
+
+        System.out.println(isDentroDaLeiMaiorQueTrinta);
+        System.out.println(isDentroDaLeiMenorQueTrinta);
+
+        //Atribuição ->   =, +=, -=, *=, %=, /=
+
+        double bonus = 1800;
+        bonus += 1000;
+        bonus -= 1000;
+        bonus *= 2;
+        bonus %= 7;
+        System.out.println(bonus);
+
+        // ++ --
+
+        int contador = 0;
+        System.out.println(contador++); //contador aqui sera impresso como 0
+        System.out.println(contador); //aqui será impresso como 1, uma vez que o ++ está na frente na linha acima
+        System.out.println(++contador);//aqui será impresso 2, uma vez q o ++ está antes do termo
 
     }
 }
